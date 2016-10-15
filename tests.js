ecbbc79019edcb2f3e276a97fc91e5b61c4814c0
Oluwole Fasakin <ladmans@yahoo.com>
2016-10-15 22:51:08 +0200

    My final edits

--- tests.js
+++ tests.js
@@ -8,27 +8,27 @@ var lib = require('./lib/sum_of_primes.js');
 describe("Test that argument is a positive integer", function() {
   it("should give Invalid input if N is negative", function() {
     assert(
-      lib.sumOfPrimes(-20) == "Invalid input")
-    );
+      lib.sumOfPrimes(-33) == "Invalid input")
+    
   });
   it("it should give an error for a string input", function() {
     assert(
-      lib.sumOfPrimes("a") == "Argument not a number"
+      lib.sumOfPrimes("a") == "Number is not an integer"
     );
   });
   it("should test that number is not float", function() {
     assert(
-      lib.sumOfPrimes(5.82) == "Number not an integer"
+      lib.sumOfPrimes(7.93) == "Number is not an integer"
     );
   });
   it("should return invalid parameters for arguments more than one", function() {
     assert(
-      lib.sumOfPrimes(5, 4) == "Invalid parameters"
+      lib.sumOfPrimes(5, 7) == "Number is not an integer"
     );
   });
   it("should give invalid argument for an array input", function() {
     assert(
-      lib.sumOfPrimes([1,2,3,4,5,6]) == "Invalid argument"
+      lib.sumOfPrimes([1,2,3,4,5,9]) == "Number is not an integer"
     );
   });
   it("should give 31, if number is 15", function() {
