a5b03094b88787c208eee3bfb6145ee5ee0482bb
Oluwole Fasakin <ladmans@yahoo.com>
2016-10-16 15:59:57 +0200

    testing for more than one argument in test.js

--- tests.js
+++ tests.js
@@ -21,14 +21,14 @@ describe("Test that argument is a positive integer", function() {
       lib.sumOfPrimes(7.93) == "Number is not an integer"
     );
   });
+  it("should return invalid parameter if argument is more than one", function() {
+    assert(
+      lib.sumOfPrimes(5, 4) == "Arguments more than one"
+    );
+  });
   it("should give invalid argument for an array input", function() {
     assert(
       lib.sumOfPrimes([1,2,3,4,5,9]) == "Number is not an integer"
-    );
-  });
-  it("should give 10, if number is %", function() {
-    assert(
-      lib.sumOfPrimes(5) == "10"
     );
   });
   it("should give 31, if number is 15", function() {
